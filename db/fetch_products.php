2969c0c9c9f44d57d38775b5862af545f85f29c5
Kaushallrai <kaushallrai03@gmail.com>
2023-05-09 13:30:03 +0545

    fetch product

---
+++ db/fetch_products.php
@@ -0,0 +1,46 @@
+<?php
+// Connect to database
+$conn = mysqli_connect("localhost", "myadmin", "myadmin", "inventory_management_system");
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Fetch data from database
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+// Display data in HTML table format
+if ($result->num_rows > 0) {
+  echo "<table>";
+  echo "<tr>";
+  echo "<th>Product ID</th>";
+  echo "<th>Product Name</th>";
+  echo "<th>Price</th>";
+  echo "<th>Quantity</th>";
+  echo "<th>Supplier</th>";
+  echo "<th>Inventory</th>";
+  echo "<th>Description</th>";
+  echo "</tr>";
+
+  while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $row['product_id'] . "</td>";
+    echo "<td>" . $row['product_name'] . "</td>";
+    echo "<td>" . $row['price'] . "</td>";
+    echo "<td>" . $row['quantity'] . "</td>";
+    echo "<td>" . $row['supplier_name'] . "</td>";
+    echo "<td>" . $row['inventory_name'] . "</td>";
+    echo "<td>" . $row['description'] . "</td>";
+    echo "</tr>";
+  }
+
+  echo "</table>";
+} else {
+  echo "0 results";
+}
+
+// Close connection
+$conn->close();
+?>
--- js/add_product.js
+++ js/add_product.js
@@ -18,19 +18,6 @@ const form = document.querySelector(".popup form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const supplierName = document.querySelector("#supplier-name").value;
-  const photo = document.querySelector("#photo").value;
-  const number = document.querySelector("#number").value;
-  const productId = document.querySelector("#product-id").value;
-  const productName = document.querySelector("#product-name").value;
-  const quantity = document.querySelector("#quantity").value;
-  const price = document.querySelector("#price").value;
-  const inventoryName = document.querySelector("#inventory-name").value;
-  const description = document.querySelector("#description").value;
-
-  // Do something with the product data, such as adding it to a list or sending it to a server using AJAX
-
-  // Hide the pop-up and overlay
   popup.style.display = "none";
   overlay.style.display = "none";
 
