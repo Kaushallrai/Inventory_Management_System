05d428557bcbb63f3dd2bef96b54fd3515bdca8b
uzwa-l <brishav59@gmail.com>
2023-05-16 02:15:02 +0545

    fetch_bill.php

--- db/fetch_bill.php
+++ db/fetch_bill.php
@@ -13,20 +13,18 @@ $result = mysqli_query($conn, $query);
  echo '<h1>Bills</h1>';
 // Display bill data
 while ($row = mysqli_fetch_assoc($result)) {
- 
     echo '<div class="bill">';
-    echo '<p>Bill ID: ' . $row['bill'] . '</p>';
+    echo '<p>Bill ID: ' . $row['bill_id'] . '</p>';
     echo '<p>Product ID: ' . $row['product_id'] . '</p>';
     echo '<p>Product Name: ' . $row['product_name'] . '</p>';
     echo '<p>Date: ' . $row['date'] . '</p>';
     echo '<p>Customer: ' . $row['customer'] . '</p>';
-    echo '<p>Destination: ' . $row['location'] . '</p>';
+    echo '<p>Destination: ' . $row['destination'] . '</p>';
     echo '<p>Quantity: ' . $row['quantity'] . '</p>';
-    echo '<p>Total Price: ' . $row['total_price'] . 
-    echo '<pStatus: ' . $row['status'] . '</p>';
+    echo '<p>Total Price: ' . $row['total_price'] . '</p>';
+    echo '<p>Status: ' . $row['status'] . '</p>';
     echo '<button onclick="printBill()">Print</button>';
-    echo '</div>';
-}
+    echo '</div>';}
 
 // Close the database connection
 mysqli_close($conn);
